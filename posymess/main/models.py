970d572ee91773bf5e178c8687737341f592d22d
Вадим Голов <grafit-design@inbox.ru>
2025-01-28 16:28:55 +0500

    Создал миграции

--- posymess/main/models.py
+++ posymess/main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+# id: int = db.Column(db.Integer, primary_key=True)
+# username: str = db.Column(db.String(20), unique=True, nullable=False)
+# email: str = db.Column(db.String(120), unique=True, nullable=False)
+# password: str = db.Column(db.String(60), nullable=False)
+
+
+class User(models.Model):
+    username: str = models.CharField('Имя пользователя', max_length=50, unique=True, null=False)
+    email: str = models.EmailField('Электронная почта', max_length=120, unique=True, null=False)
+    password: str = models.CharField('Пароль', max_length=60, null=False)
+
+    def __str__(self):
+        return self.username
+
+
+class Flower(models.Model):
+    posy_name: str = models.CharField('Название букета', max_length=50, unique=True, null=False)
+    price: float = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.posy_name
+
+
+class Order(models.Model):
+    user: str = models.ForeignKey(User, on_delete=models.CASCADE)
+    flower: str = models.ForeignKey(Flower, on_delete=models.CASCADE)
+    order_date: str = models.DateTimeField('Дата заказа', auto_now_add=True)
+
+    def __str__(self):
+        return f'Order #{self.id} by {self.user.username} for {self.flower.posy_name}'
--- posymess/main/views.py
+++ posymess/main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import User, Flower, Order
 
 
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+def flower_list(request):
+    flowers = Flower.objects.all()
+    return render(request, 'flower_list.html', {'flowers': flowers})

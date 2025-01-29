005c4d857c024036dc1ae10a0c971bef61267e90
Вадим Голов <grafit-design@inbox.ru>
2025-01-29 21:52:06 +0500

    Сделал регистрацию

--- posymess/main/forms.py
+++ posymess/main/forms.py
@@ -5,9 +5,9 @@ from django.forms import ModelForm, TextInput, EmailInput, PasswordInput
 class RegisterForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['user_name', 'email', 'password']
         widgets = {
-            'username': TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите имя пользователя', 'value': ''}),
+            'user_name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите имя пользователя', 'value': ''}),
             'email': EmailInput(attrs={'class': 'form-control', 'placeholder': 'Введите электронную почту', 'value': ''}),
             'password': PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Введите пароль', 'value': ''})
         }
--- posymess/main/models.py
+++ posymess/main/models.py
@@ -1,18 +1,13 @@
 from django.db import models
 
-# id: int = db.Column(db.Integer, primary_key=True)
-# username: str = db.Column(db.String(20), unique=True, nullable=False)
-# email: str = db.Column(db.String(120), unique=True, nullable=False)
-# password: str = db.Column(db.String(60), nullable=False)
-
 
 class User(models.Model):
-    username: str = models.CharField('Имя пользователя', max_length=50, unique=True, null=False)
+    user_name: str = models.CharField('Имя пользователя', max_length=50, unique=True, null=False)
     email: str = models.EmailField('Электронная почта', max_length=120, unique=True, null=False)
     password: str = models.CharField('Пароль', max_length=60, null=False)
 
     def __str__(self):
-        return self.username
+        return self.user_name
 
     class Meta:
         verbose_name = 'Пользователь'
@@ -37,7 +32,7 @@ class Order(models.Model):
     order_date: str = models.DateTimeField('Дата заказа', auto_now_add=True)
 
     def __str__(self):
-        return f'Заказ №{self.id} от {self.user.username} букет {self.flower.posy_name}'
+        return f'Заказ №{self.id} от {self.user.user_name} букет {self.flower.posy_name}'
 
     class Meta:
         verbose_name = 'Заказ'

cc3da60d7f118effb5ae767b30530fd71ddf8523
Вадим Голов <grafit-design@inbox.ru>
2025-02-03 01:39:15 +0500

    Удалил лишний код

--- posymess/main/forms.py
+++ posymess/main/forms.py
@@ -1,6 +1,5 @@
 import re
-from django.db import IntegrityError
-from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.hashers import check_password
 from .models import User
 
 
@@ -154,68 +153,3 @@ def check_auth(log_data) -> int | User:
         return 0
     else:
         return email
-
-
-
-
-
-
-
-
-
-# user_name = forms.CharField(max_length=50, required=True, label='Имя пользователя')
-# email = forms.EmailField(max_length=120, required=True, label='Электронная почта')
-# password = forms.CharField(widget=forms.PasswordInput, required=True, label='Пароль', min_length=3)
-# confirm_password = forms.CharField(widget=forms.PasswordInput, required=True, label='Подтвердите пароль')
-#
-# def clean(self) -> dict[str, str]:
-#     cleaned_data = super().clean()
-#
-#     user_name = cleaned_data.get('user_name')
-#     email = cleaned_data.get('email')
-#     password = cleaned_data.get('password')
-#     confirm_password = cleaned_data.get('confirm_password')
-#
-#     if password != confirm_password:
-#         raise forms.ValidationError('Пароли не совпадают.')
-#     else:
-#         cleaned_data['password'] = make_password(password)
-#
-#     return cleaned_data
-
-# def __init__(self, raw_data: dict | None = None) -> None:
-#     self.raw_data = raw_data or {}
-#     self.errors = {}
-#     self.cleaned_data = {}
-#
-# def validation(self):
-#     username = self.raw_data.get('username', '')
-#     email = self.raw_data.get('email', '')
-#     password = self.raw_data.get('password', '')
-#     password_confirm = self.raw_data.get('password_confirm', '')
-#
-#     if not username:
-#         self.errors['username'] = 'Имя пользователя обязательно.'
-#     elif len(username) < 3:
-#         self.errors['username'] = 'Имя пользователя должно содержать минимум 3 символа.'
-#
-#     if not email:
-#         self.errors['email'] = 'Электронная почта обязательна.'
-#     elif forms.EmailAddressField().clean(email).errors():
-#         self.errors['email'] = 'Электронная почта должна содержать минимум 3 символа.'
-#
-#     if not password:
-#         self.errors['password'] = 'Пароль обязателен.'
-#     elif len(password) < 3:
-#         self.errors['password'] = 'Пароль должен содержать не менее 3 символов.'
-#
-#     if password != password_confirm:
-#         self.errors['password_confirm'] = 'Пароли не совпадают.'
-#
-#     # Если нет ошибок, сохранить данные в cleaned_data
-#     if not self.errors:
-#         self.cleaned_data['username'] = username
-#         self.cleaned_data['password'] = make_password(password)
-#         return True
-#
-#     return False

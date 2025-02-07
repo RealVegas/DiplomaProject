06f733b6aa529ff8c3803b72e8b062e568d505b8
Вадим Голов <grafit-design@inbox.ru>
2025-02-07 16:42:15 +0500

    Написал параллельный запуск

--- start.py
+++ start.py
@@ -1,9 +1,28 @@
 import os
+import threading
+
+
+# Для запуска бота
+def run_bot():
+    os.system('python manage.py bot')
+
+
+# Для сайта
+def run_server():
+    os.system('python manage.py runserver')
+
+
+# Для тестов
+def run_test():
+    os.system('python manage.py test')
+
 
 if __name__ == '__main__':
     if os.path.exists('posymess'):
         os.chdir('posymess')
 
-    # os.system('python manage.py test')
-    # os.system('python manage.py bot')
-    os.system('python manage.py runserver')
+    start_bot = threading.Thread(target=run_bot)
+    start_server = threading.Thread(target=run_server)
+
+    start_bot.start()
+    start_server.start()
